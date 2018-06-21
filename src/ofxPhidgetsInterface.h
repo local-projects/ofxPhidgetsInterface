d5e666b4c9db4b945c731d2dda0876c5b9c8e05a
nicoleyimessier <nicoleyimessier@gmail.com>
2018-06-21 16:13:57 -0400

    simple ofxphidgets interface

---
+++ src/ofxPhidgetsInterface.h
@@ -0,0 +1,41 @@
+//
+//  ofxPhidgetsInterface.cpp
+//  example
+//
+//  Created by Nicole Messier on 6/21/18.
+//
+//
+
+#include "ofMain.h"
+#include <phidget22.h>
+#include <PhidgetHelperFunctions.h>
+
+class ofxPhidgetsInterface{
+protected:
+    /*
+     SOME NOTES ON PHIDGETS:
+     
+     * Helpful link: https://www.phidgets.com/docs/Phidget_Programming_Basics
+     
+     * CCONV stands for "Calling Convention", and it's just a defined term in our libraries that makes sure the function is called with the proper prefix for the compiler that's being used. You can find CCONV defined in phidget22.h
+     
+     */
+    static void CCONV onAttachHandler(PhidgetHandle ph, void *ctx);
+    static void CCONV onDetachHandler(PhidgetHandle ph, void *ctx);
+    static void CCONV onErrorHandler(PhidgetHandle phid, void *ctx, Phidget_ErrorEventCode errorCode, const char *errorString);
+    static void CCONV onVoltageRatioChangeHandler(PhidgetVoltageRatioInputHandle pvih, void *ctx, double voltage);
+    static void CCONV onSensorChangeHandler(PhidgetVoltageRatioInputHandle pvih, void *ctx, double sensorValue, Phidget_UnitInfo *unitInfo);
+    void PrintEventDescriptions();
+    static void CCONV onVoltageChangeHandler(PhidgetVoltageRatioInputHandle voltageInput, void *ctx, double voltage);
+    
+public:
+    ofxPhidgetsInterface();
+    ~ofxPhidgetsInterface();
+    
+    void setup();
+    void update();
+    
+private:
+    PhidgetVoltageRatioInputHandle ch = NULL;
+
+};
